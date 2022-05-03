814f273ecbba637e473fd52d27ea28b23121ed49
rlmarlma <102306488+rlmarlma@users.noreply.github.com>
2022-05-03 13:19:17 +0300

    Delete KotlinHomeworkTask3 directory

--- KotlinHomeworkTask3/src/File.kt
+++
@@ -1,6 +0,0 @@
-class File(override var ParentObject: String, override var name: String, var extension: String) : AbstractFileSystemNode() {
-
-    override fun returnExtension() {
-        println(extension)
-    }
-}
